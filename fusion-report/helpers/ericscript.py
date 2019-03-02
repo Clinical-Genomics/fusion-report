d3a262a5b46c981f864e62afedf4e8cf29623f4b
Martin Proks <mproksik@gmail.com>
2019-03-02 14:52:18 +0100

    Added Ericscript parser

---
+++ fusion-report/helpers/ericscript.py
@@ -0,0 +1,27 @@
+class Ericscript():
+    def __init__(self, file):
+        self.__fusions = {}
+        self.parse(file)
+
+    def parse(self, file):
+        try:
+            with open(file, 'r') as in_file:
+                next(in_file) # skip header
+                for row in in_file:
+                    col = row.split('\t')
+                    fusion = f"{col[0]}--{col[1]}"
+                    if fusion not in self.__fusions:
+                        self.__fusions[fusion] = {
+                            'position': f"{col[2]}:{col[3]}:{col[4]}#{col[5]}:{col[6]}:{col[7]}",
+                            'discordant_reads': int(col[10]),
+                            'junction_reads': int(col[11]),
+                            'fusion_type': col[14],
+                            'gene_expr1': float(col[18]),
+                            'gene_expr2': float(col[19]),
+                            'gene_expr_fusion': float(col[20])
+                        }
+        except IOError as error:
+            print(error)
+    
+    def get_fusions(self):
+        return self.__fusions
