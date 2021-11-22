3e4b787dbb86879ae02634fbb619bb67ba43c3e6
Mike Lloyd <mwlloyd1@gmail.com>
2021-11-22 15:44:50 -0500

    Update jaffa.py

--- fusion_report/parsers/jaffa.py
+++ fusion_report/parsers/jaffa.py
@@ -10,38 +10,31 @@ class Jaffa(AbstractFusionTool):
         self.header: List[str] = header.strip().split(delimiter)
 
     def parse(self, line: str, delimiter: Optional[str] = ',') -> List[Tuple[str, Dict[str, Any]]]:
-        
-        #print(self.header)
-
         col: List[str] = [x.strip() for x in line.split(delimiter)]
 
-        #print(type(col[self.header.index('fusion.genes')]))
-        #print(type(col[self.header.index('fusion.genes')].split(':')))
-        
         fusions = col[self.header.index('fusion genes')].split(':')
 
         fusion: str = '--'.join([fusions[0], fusions[1]])
 
         details: Dict[str, Any] = {
-            'position': 
-
-            "#".join([
-                ':'.join([col[self.header.index('chrom1')],
-                         col[self.header.index('base1')],
-                         col[self.header.index('strand1')]
-                         ]).replace('chr', ''),
-                ':'.join([col[self.header.index('chrom2')],
-                         col[self.header.index('base2')],
-                         col[self.header.index('strand2')]
-                         ]).replace('chr', '')
-                ]),
-            'spanning_pairs': int(col[self.header.index('spanning pairs')]),
-            'spanning_reads': int(col[self.header.index('spanning reads')]),
-            'inframe': col[self.header.index('inframe')].strip(),
-            'aligns': col[self.header.index('aligns')].strip(),
-            'rearrangement': col[self.header.index('rearrangement')].strip(),
-            'classification': col[self.header.index('classification')].strip(),
-            'known': col[self.header.index('known')].strip()
-        }
+                                    'position': 
+                                        "#".join([
+                                                ':'.join([col[self.header.index('chrom1')],
+                                                        col[self.header.index('base1')],
+                                                        col[self.header.index('strand1')]
+                                                        ]).replace('chr', ''),
+                                                ':'.join([col[self.header.index('chrom2')],
+                                                        col[self.header.index('base2')],
+                                                        col[self.header.index('strand2')]
+                                                        ]).replace('chr', '')
+                                                ]),
+                                        'spanning_pairs': int(col[self.header.index('spanning pairs')]),
+                                        'spanning_reads': int(col[self.header.index('spanning reads')]),
+                                        'inframe': col[self.header.index('inframe')].strip(),
+                                        'aligns': col[self.header.index('aligns')].strip(),
+                                        'rearrangement': col[self.header.index('rearrangement')].strip(),
+                                        'classification': col[self.header.index('classification')].strip(),
+                                        'known': col[self.header.index('known')].strip()
+                                    }
 
         return [(fusion, details)]
