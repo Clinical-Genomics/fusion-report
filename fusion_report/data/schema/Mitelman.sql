6f114f78eb2796f5792891ebb50f0de3c1d3b6bc
Annick Renevey <47788523+rannick@users.noreply.github.com>
2022-03-04 11:32:56 +0100

    update mitelman: fist attempts

--- fusion_report/data/schema/Mitelman.sql
+++ fusion_report/data/schema/Mitelman.sql
@@ -1,10 +1,25 @@
-CREATE TABLE AuthorReference (
+CREATE TABLE AUTHREF (
 	Refno int NOT NULL ,
 	Name varchar2 (100) NOT NULL ,
 	NameOrder smallint NOT NULL 
-);
+)
+;
 
-CREATE TABLE Cytogen (
+CREATE TABLE CYTINV (
+	RefNo int NOT NULL ,
+	CaseNo varchar2 (14) NOT NULL ,
+	InvNo smallint NOT NULL ,
+	InvDate varchar2 (8) NULL ,
+	Tissue varchar2 (10) NULL ,
+	Clones smallint NULL ,
+	KaryLength smallint NULL ,
+	KaryShort varchar2 (255) NULL ,
+	KaryLong varchar2(4000) NULL ,
+	CaseOrder varchard (200) NULL
+)
+;
+
+CREATE TABLE CYTOGEN (
 	RefNo int NOT NULL ,
 	CaseNo varchar2 (14) NOT NULL ,
 	Sex char (1) NULL ,
@@ -23,23 +38,24 @@ CREATE TABLE Cytogen (
 	Topo varchar2 (20) NULL ,
 	Immunology char (1) NULL ,
 	SpecObs varchar2 (255) NULL ,
-	Origin char (1) NULL
-);
-
-CREATE TABLE CytogenInv (
-	RefNo int NOT NULL ,
-	CaseNo varchar2 (14) NOT NULL ,
-	InvNo smallint NOT NULL ,
-	InvDate varchar2 (8) NULL ,
-	Tissue varchar2 (10) NULL ,
-	Clones smallint NULL ,
-	KaryLength smallint NULL ,
-	KaryShort varchar2 (255) NULL ,
-	KaryLong varchar2(4000) NULL
+	Origin varchar2 (20) NULL
 )
 ;
 
-CREATE TABLE KaryAbnorm (
+
+CREATE TABLE CYTOVAL (
+	RefNo int NOT NULL ,
+	CaseNo varchar2 (14) NOT NULL ,
+	InvNo smallint NOT NULL ,
+	KaryLength smallint NULL ,
+	KaryShort varchar (255) NULL ,
+	KaryLong varchar2(4000) NULL ,
+	Message varchar2 (4000) NULL ,
+	Reason varchar2 (4000) NULL 
+)
+;
+
+CREATE TABLE KABNORM (
 	RefNo int NOT NULL ,
 	CaseNo varchar2 (14) NOT NULL ,
 	InvNo smallint NOT NULL ,
@@ -49,7 +65,7 @@ CREATE TABLE KaryAbnorm (
 )
 ;
 
-CREATE TABLE KaryBit (
+CREATE TABLE KBIT (
 	RefNo int NOT NULL ,
 	CaseNo varchar2 (14) NOT NULL ,
 	InvNo smallint NOT NULL ,
@@ -59,7 +75,7 @@ CREATE TABLE KaryBit (
 )
 ;
 
-CREATE TABLE KaryBreak (
+CREATE TABLE KBREAK (
 	RefNo int NOT NULL ,
 	CaseNo varchar2 (14) NOT NULL ,
 	InvNo smallint NOT NULL ,
@@ -69,9 +85,9 @@ CREATE TABLE KaryBreak (
 )
 ;
 
-CREATE TABLE KaryClone (
+CREATE TABLE KCLONE (
 	RefNo int NOT NULL ,
-	CaseNo varchar2 (14) NOT NULL ,
+	CaseNo varchar2 (14) NOT NULL ,:q
 	InvNo smallint NOT NULL ,
 	CloneNo smallint NOT NULL ,
 	ChromoMin smallint NOT NULL ,
@@ -79,11 +95,11 @@ CREATE TABLE KaryClone (
 	CloneShort varchar2 (255) NOT NULL ,
 	CloneLong varchar2(4000) NULL ,
 	CloneLength int NOT NULL ,
-	SoleAbnorm char (1) NULL
+	Sole char (1) NULL
 )
 ;
 
-CREATE TABLE Koder (
+CREATE TABLE KODER (
 	Kod varchar2 (16) NOT NULL ,
 	KodTyp varchar2 (16) NOT NULL ,
 	Aktiv char (1) NOT NULL ,
@@ -91,15 +107,22 @@ CREATE TABLE Koder (
 	Benamning varchar2 (255) NOT NULL ,
 	Kortnamn varchar2 (30) NULL ,
 	Notering varchar2 (255) NULL 
+	Inregdatum varchar2 (255) NULL 
+	GiltigFran varchar2 (255) NULL 
+	GiltigTill varchar2 (255) NULL 
+	Flagga1 varchar2 (255) NULL 
+	Flagga2 varchar2 (255) NULL 
+	Flagga3 varchar2 (255) NULL 
+	Flagga4 varchar2 (255) NULL 
 )
 ;
 
-CREATE TABLE MolBiolClinAssoc (
+CREATE TABLE MBCA
 	MolClin char (1) NOT NULL ,
 	RefNo int NOT NULL ,
 	InvNo smallint NOT NULL ,
 	Morph varchar (20) NULL ,
-	Top varchar (20) NULL ,
+	Topo varchar (20) NULL ,
 	Immunology char (1) NULL ,
 	GeneLength smallint NULL ,
 	GeneShort varchar (255) NULL ,
@@ -107,35 +130,63 @@ CREATE TABLE MolBiolClinAssoc (
 	KaryLength smallint NULL ,
 	KaryShort varchar (255) NULL ,
 	KaryLong varchar2(4000) NULL 
-);
+)
+;
 
-CREATE TABLE MolClinAbnorm (
+CREATE TABLE MCABNORM (
 	MolClin char (1) NOT NULL ,
 	RefNo int NOT NULL ,
 	InvNo smallint NOT NULL ,
 	OrderNo smallint NOT NULL ,
 	Abnormality varchar (100) NOT NULL 
-);
+)
+;
 
-CREATE TABLE MolClinBreak (
+CREATE TABLE MCBREAK (
 	MolClin char (1) NOT NULL ,
 	RefNo int NOT NULL ,
 	InvNo smallint NOT NULL ,
 	OrderNo smallint NOT NULL ,
 	Breakpoint varchar (10) NOT NULL 
-);
+)
+;
 
-CREATE TABLE MolClinGene (
+CREATE TABLE MCGENE (
 	MolClin char (1) NOT NULL ,
 	RefNo int NOT NULL ,
 	InvNo smallint NOT NULL ,
 	OrderNo smallint NOT NULL ,
 	Prefix char (2) ,
 	Gene varchar (16) NOT NULL ,
-	Suffix char (2) 
-);
+	Suffix char (2)
+)
+;
 
-CREATE TABLE Reference (
+CREATE TABLE RECAB (
+	Chromosome varchar (255) NULL ,
+	Arm varchar (255) NULL ,
+	Band varchar (255) NULL ,
+	Abnormality varchar (255) NULL ,
+	Morph varchar (255) NULL ,
+	Topo varchar (255) NULL ,
+	TotalCases varchar (255) NULL ,
+	Gene varchar (255) NULL ,
+	ChrOrder varchar (255) NULL ,
+	Type varchar (255) NULL
+)
+;
+
+CREATE TABLE RECABNUM (
+	Chromosome varchar (255) NULL ,
+	Abnormality varchar (255) NULL ,
+	Morph varchar (255) NULL ,
+	Topo varchar (255) NULL ,
+	TotalCases varchar (255) NULL ,
+	ChrOrder varchar (255) NULL
+)
+;
+
+CREATE TABLE REF (
 	Refno NUMBER(9) NOT NULL ,
 	TitleLength NUMBER(5) NULL ,
 	TitleShort varchar2 (255) NULL ,
@@ -144,35 +195,11 @@ CREATE TABLE Reference (
 	Year NUMBER(4) NULL ,
 	Journal varchar2 (255) NULL ,
 	Text varchar2(4000) NULL ,
-	Abbreviation varchar2 (100) NULL ,
+	Abbreviation varchar2 (255) NULL ,
 	AuthorsLength NUMBER(5) NULL ,
 	AuthorsShort varchar2 (255) NULL ,
 	AuthorsLong varchar2(4000) NULL ,
 	Flag varchar2 (1) NULL,
-	PubMed NUMBER 
-)
-;
-
-CREATE TABLE RECURRENT_DATA (
-   CHROMOSOME                                         VARCHAR2(5) ,
-   ARM                                                VARCHAR2(2) ,
-   BAND                                               NUMBER ,
-   ABERRATION                                         VARCHAR2(100) ,
-   CODE                                               VARCHAR2(10) ,
-   ORGAN                                              VARCHAR2(10) ,
-   TOTAL_CASES                                        VARCHAR2(5) ,
-   GENE                                               VARCHAR2(100) ,
-   CHR_ORDER                                          NUMBER ,
-   TYPE                                               CHAR(1)
-)
-;
-
-CREATE TABLE RECURRENT_NUM_DATA (
-   CHROMOSOME                                         VARCHAR2(5) ,
-   ABNORMALITY                                        VARCHAR2(5) ,
-   CODE                                               VARCHAR2(10) ,
-   ORGAN                                              VARCHAR2(10) ,
-   TOTAL_CASES                                        VARCHAR2(5) ,
-   CHR_ORDER                                          NUMBER
+	PubMed varchar2 (255) NULL
 )
 ;
